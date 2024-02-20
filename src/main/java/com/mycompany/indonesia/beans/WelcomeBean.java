9e44c35e6d4c4567d3e28c061eb4c3d963846bcd
Victory <victorykasende@gmail.com>
2024-02-20 13:38:22 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/indonesia/beans/WelcomeBean.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.indonesia.beans;
+
+/**
+ *
+ * @author Mk
+ */
+import java.io.Serializable;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+@Named("welcomeBean")
+@RequestScoped
+public class WelcomeBean implements Serializable {
+
+    private String name;
+    private String welcomeMessage;
+    static boolean commence=false;
+
+    public WelcomeBean() {
+
+    }
+
+
+    public  boolean getCommence(){
+        return commence;
+    }
+
+    public String getWelcomeMessage() {
+        return welcomeMessage;
+    }
+    public void sayWelcome() {
+
+        if (!commence) {
+            welcomeMessage = "Selamat datang, " + name + "!";
+            commence = true;
+        }
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String commencer() {
+        commence=false;
+        return "page_accueil";
+    }
+}
